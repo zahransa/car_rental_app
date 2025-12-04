ad0b6b497011f91843e5285d36dc5e35e612ace1
User <saeedzahran@hotmail.com>
2025-12-04 20:28:19 +0100

    Initial car rental app

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CarRentalApp());
+}
+
+class CarRentalApp extends StatelessWidget {
+  const CarRentalApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Car Rental',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const CarListScreen(),
+    );
+  }
+}
+
+class CarListScreen extends StatelessWidget {
+  const CarListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final cars = [
+      {'name': 'Toyota Corolla', 'price': '45 €/day'},
+      {'name': 'Volkswagen Golf', 'price': '55 €/day'},
+      {'name': 'BMW 3 Series', 'price': '90 €/day'},
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Available Cars'),
+        centerTitle: true,
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(16),
+        itemCount: cars.length,
+        separatorBuilder: (_, __) => const SizedBox(height: 12),
+        itemBuilder: (context, index) {
+          final car = cars[index];
+          return Card(
+            elevation: 2,
+            child: ListTile(
+              title: Text(car['name']!),
+              subtitle: Text(car['price']!),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {},
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        icon: const Icon(Icons.add),
+        label: const Text('Add Car'),
+      ),
+    );
+  }
+}
